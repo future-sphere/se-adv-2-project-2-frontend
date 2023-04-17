8c22cd18dbf2cc760c70ffefaef9f8752a8097b0
mxdi9i7 <peter.zheng88228@gmail.com>
2023-04-16 22:14:07 -0400

    wip

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -12,39 +12,11 @@ import a from '../../services';
 
 type Props = {};
 
-const orders = [
-  {
-    number: 'WU88191111',
-    href: '#',
-    invoiceHref: '#',
-    createdDate: 'Jul 6, 2021',
-    createdDatetime: '2021-07-06',
-    deliveredDate: 'July 12, 2021',
-    deliveredDatetime: '2021-07-12',
-    total: '$160.00',
-    products: [
-      {
-        id: 1,
-        name: 'Micro Backpack',
-        description:
-          'Are you a minimalist looking for a compact carry option? The Micro Backpack is the perfect size for your essential everyday carry items. Wear it like a backpack or carry it like a satchel for all-day use.',
-        href: '#',
-        price: '$70.00',
-        imageSrc:
-          'https://tailwindui.com/img/ecommerce-images/order-history-page-03-product-01.jpg',
-        imageAlt:
-          'Moss green canvas compact backpack with double top zipper, zipper front pouch, and matching carry handle and backpack straps.',
-      },
-      // More products...
-    ],
-  },
-  // More orders...
-];
-
 const ProfilePage = (props: Props) => {
   const [orders, setOrders] = React.useState<Order[]>([]);
-  const [userId, setUserId] = React.useState<number>(0);
-  const navigate = useNavigate();
+
+  const userId = localStorage.getItem('currentUserId');
+
   useEffect(() => {
     const fetchData = async () => {
       const response = await a.get('/orders/student/' + userId);
@@ -52,32 +24,8 @@ const ProfilePage = (props: Props) => {
         setOrders(response.data);
       }
     };
-    const checkAuth = async () => {
-      const token = localStorage.getItem('token');
-      if (!token) {
-        return navigate('/signin');
-      }
-
-      try {
-        const response = await a.get('/auth/check', {
-          params: {
-            token,
-          },
-        });
-
-        if (!response.data.id) {
-          return navigate('/signin');
-        }
-        setUserId(response.data.id);
-      } catch (error: any) {
-        if (error.response.status === 401) {
-          return navigate('/signin');
-        }
-      }
-    };
 
     fetchData();
-    checkAuth();
   }, []);
 
   return (
